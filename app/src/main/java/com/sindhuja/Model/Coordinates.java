257c3a863bad0964e1b48de4511ec1a74745e70d
SindhujaG <gadde.sindhuja@gmail.com>
2020-04-22 20:54:38 -0400

    Adding files
    Adding files

---
+++ app/src/main/java/com/sindhuja/Model/Coordinates.java
@@ -0,0 +1,31 @@
+package com.sindhuja.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Coordinates implements Serializable {
+    @SerializedName("lon")
+    @Expose
+    private Double lon;
+    @SerializedName("lat")
+    @Expose
+    private Double lat;
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+}
---
+++ app/src/main/java/com/sindhuja/listener/OnClickListener.java
@@ -0,0 +1,8 @@
+package com.sindhuja.listener;
+
+
+import com.sindhuja.Model.City;
+
+public interface OnClickListener {
+    void onItemClicked(int pos, City city);
+}
